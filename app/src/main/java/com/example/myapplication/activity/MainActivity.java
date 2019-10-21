5f0046159059a94968b34a2c02c400e41bf05e0e
Dmitry Rudenko <drudenk@roox.ru>
2019-10-21 12:24:58 +0300

    Поиск заклинаний по имени

--- app/src/main/java/com/example/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
     }
 
     @Override
