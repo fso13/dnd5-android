65690dd780093ce2d6c6d46d625555ec122707e9
LaughingMan <13laughingman13@gmail.com>
2019-10-23 23:16:30 +0300

    правка названий заклинаний

--- app/src/main/java/com/example/myapplication/service/SpellService.java
+++ app/src/main/java/com/example/myapplication/service/SpellService.java
@@ -9,13 +9,17 @@ import com.squareup.moshi.Types;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class SpellService {
+    private static final Pattern PATTERN = Pattern.compile("[А-я]", Pattern.MULTILINE);
     private static Type listSpellType = Types.newParameterizedType(List.class, Spell.class);
-    private static Type mapStrinfSpellsType = Types.newParameterizedType(Map.class, Clazz.class, ClassInfo.class);
+    private static Type mapStringSpellsType = Types.newParameterizedType(Map.class, Clazz.class, ClassInfo.class);
 
     public static List<Spell> getAllSpells(String json) throws IOException {
         Moshi moshi = new Moshi.Builder().build();
@@ -27,7 +31,29 @@ public class SpellService {
     public static Map<Clazz, ClassInfo> getClassSpells(String json) throws IOException {
         Moshi moshi = new Moshi.Builder().build();
 
-        JsonAdapter<Map<Clazz, ClassInfo>> jsonAdapter = moshi.adapter(mapStrinfSpellsType);
+        JsonAdapter<Map<Clazz, ClassInfo>> jsonAdapter = moshi.adapter(mapStringSpellsType);
         return jsonAdapter.fromJson(json);
     }
+
+    public static List<Spell> getAllSpells(List<Spell> spells, String txt) {
+
+        final Map<String, String> map = new HashMap<>();
+        for (String s : txt.split("\n")) {
+            Matcher m = PATTERN.matcher(s);
+            if (m.find()) {
+                map.put(s.substring(0, m.toMatchResult().start()).trim(), s.substring(m.toMatchResult().start()));
+            }
+        }
+
+        for (final Spell spell : spells) {
+            String s = map.get(spell.getEn().getName().toUpperCase());
+            if (s != null) {
+                spell.getRu().setName(s);
+            } else {
+                spell.getRu().setName(spell.getRu().getName().toUpperCase());
+            }
+        }
+        return spells;
+    }
+
 }
