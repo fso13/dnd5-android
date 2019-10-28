2f912e0d2ea951f028b4c344617461266ec5a332
LaughingMan <13laughingman13@gmail.com>
2019-10-28 23:52:32 +0300

    фильтр монстров

---
+++ app/src/main/java/com/example/myapplication/model/monster/Biom.java
@@ -0,0 +1,43 @@
+package com.example.myapplication.model.monster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Biom {
+    All("Все"),
+    ARCTIC("Заполярье"),
+    COASTAL("Побережье"),
+    DESERT("Пустыня"),
+    FOREST("Лес"),
+    GRASSLAND("Равнина"),
+    HILL("Холмы"),
+    MOUNTAIN("Горы"),
+    SWAMP("Болота"),
+    UNDERDARK("Подземье"),
+    URBAN("Город"),
+
+    ;
+
+    private String ru;
+
+    Biom(String ru) {
+        this.ru = ru;
+    }
+
+    public static Biom fromRu(String ru) {
+        for (Biom clazz : Biom.values()) {
+            if (clazz.ru.equals(ru)) {
+                return clazz;
+            }
+        }
+        return null;
+    }
+
+    public static List<String> getRu() {
+        List<String> list = new ArrayList<>();
+        for (Biom clazz : Biom.values()) {
+            list.add(clazz.ru);
+        }
+        return list;
+    }
+}
