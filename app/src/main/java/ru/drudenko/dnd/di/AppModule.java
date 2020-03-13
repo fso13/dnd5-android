ef90c9abfdf57118e12d6d1c2e5fc0fabd856781
LaughingMan <13laughingman13@gmail.com>
2020-03-13 13:30:44 +0300

    2003120

--- app/src/main/java/ru/drudenko/dnd/di/AppModule.java
+++ app/src/main/java/ru/drudenko/dnd/di/AppModule.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +23,7 @@ import ru.drudenko.dnd.model.magic.ClassInfo;
 import ru.drudenko.dnd.model.magic.Clazz;
 import ru.drudenko.dnd.model.magic.Spell;
 import ru.drudenko.dnd.model.monster.Monster;
+import ru.drudenko.dnd.model.monster.Trait;
 
 
 @Module
@@ -121,6 +123,34 @@ public class AppModule {
 
             for (Monster monster : monsters) {
                 final String key = "MONSTER_" + monster.getName().replace(" ", "_");
+                if(monster.getName().equals("Колючий Дьявол (Spined Devil)")){
+                    List<Trait> traits = new LinkedList<>();
+                    Trait t1 = new Trait();
+                    t1.setName("Мультиатака");
+                    t1.setText(Collections.singletonList("Дьявол совершает две атаки: одну укусом, и одну вилами, или же две атаки иглами на хвосте"));
+                    traits.add(t1);
+
+                    Trait t2 = new Trait();
+                    t2.setName("Укус");
+                    t2.setAttack(Collections.singletonList("Рубящий урон 5 (2d4)"));
+                    t2.setText(Collections.singletonList("Рукопашная атака оружием: +2 к попаданию, досягаемость 5 фт., одна цель"));
+                    traits.add(t2);
+
+                    Trait t3 = new Trait();
+                    t3.setName("Вилы");
+                    t3.setAttack(Collections.singletonList("Колющий урон 3 (1d6)"));
+                    t3.setText(Collections.singletonList("Рукопашная атака оружием: +2 к попаданию, досягаемость 5 фт., одна цель"));
+                    traits.add(t3);
+
+
+                    Trait t4 = new Trait();
+                    t4.setName("Игла на хвосте");
+                    t4.setAttack(Collections.singletonList("Колющий урон 4 (1d4 + 2) плюс урон огнём 3 (1d6)"));
+                    t4.setText(Collections.singletonList("Дальнобойная атака оружием: +4 к попаданию, дистанция 20/80 фт., одна цель"));
+                    traits.add(t4);
+
+                    monster.setAction(traits);
+                }
                 monster.setFavorite(preferences.getBoolean(key, false));
             }
 
